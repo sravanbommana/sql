89e359a1d5bfd5964249946507ee0ae45d28fe4b
Sravan Kumar <bommana@iloguest.org>
2024-04-02 10:58:34 +0530

    releation changes

---
+++ 01-create-table.sql
@@ -0,0 +1,25 @@
+
+CREATE TABLE employees (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	first_name VARCHAR(300) NOT NULL,
+	last_name VARCHAR(300) NOT NULL,
+	birthdate DATE NOT NULL,
+-- 	email VARCHAR(200) REFERENCES intranet_accounts ON DELETE 
+	email VARCHAR(200) UNIQUE NOT NULL,
+    team_id INT REFERENCES teams ON DELETE SET NULL
+);
+CREATE TABLE intranet_accounts (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(200) REFERENCES employees (email) ON DELETE CASCADE,
+	password VARCHAR(200) NOT NULL
+);
+CREATE TABLE comapny_buildings (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE teams (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(300) NOT NULL,
+    building_id INT REFERENCEs company_buildings ON DELETE SET NULL
+)
